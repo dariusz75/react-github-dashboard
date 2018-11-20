198e976b3ced0005fb95f81e2c389bc273fbf3fe
Dariusz Franczak <darius32@op.pl>
2018-11-20 23:44:24 +0000

    basic ant.design layout implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Icon } from 'antd';
 import './App.css';
 
 import axios from 'axios';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 class App extends Component {
 
@@ -91,29 +94,38 @@ class App extends Component {
   }
 
 
-
   componentDidMount() {
     this.fetchData('dariusz75');
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Layout>
+        <Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} style={{ marginTop: '60px' }}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">Profile</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="bars" />
+              <span className="nav-text">All Repos</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout style={{ marginLeft: 200, height: '100vh' }} >
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+            <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+      </Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
